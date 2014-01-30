b64244deb383894581c5b37c11411fedebd53271
CodeBlue <codebluedev@gmail.com>
2014-01-29 23:07:44 -0700

    Adding Files
    Adding zip files and extracted contents for review. Including README
    that has most basic steps and a link to more in depth tutorial.

---
+++ NUnit Test Project/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+namespace $safeprojectname$
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        #region Set Up and Tear Down
+
+        #region Test Suite Set Up and Tear Down
+
+        /// <summary>
+        /// This runs only once at the beginning of all tests and is used for all tests in the 
+        /// class.
+        /// </summary>
+        [TestFixtureSetUp]
+        public void TestSuiteSetUp()
+        {
+
+        }
+
+        /// <summary>
+        /// This runs only once at the end of all tests and is used for all tests in the class.
+        /// </summary>
+        [TestFixtureTearDown]
+        public void TestSuiteTearDown()
+        {
+
+        }
+
+        #endregion Test Suite Set Up and Tear Down
+
+        #region Individual Test Set Up and Tear Down
+
+        /// <summary>
+        /// This setup function runs before each test method is executed.
+        /// </summary>
+        [SetUp]
+        public void TestSetUp()
+        {
+
+        }
+
+        /// <summary>
+        /// This tear down function runs after each test method is executed.
+        /// </summary>
+        [TearDown]
+        public void TestTearDown()
+        {
+            
+        }
+
+        #endregion Individual Test Set Up and Tear Down
+
+        #endregion Set Up and Tear Down
+
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+NUnit Visual Studio Templates
+==========================
+
+Custom Project and Item Templates to use with the NUnit Framework.
+
+Download and install the zip files to '%USERPROFILE%\Documents\Visual Studio 2012\Templates\\\{TemplateType\}\'
+Where TemplateType is ItemTemplate or ProjectTemplate.
+
+In depth tutorial on the creation and installation of these files can be found [here](http://codebluedev.blogspot.com/2014/01/visual-studio-2012-nunit-templates.html).
